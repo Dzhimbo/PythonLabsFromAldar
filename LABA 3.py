7ee5fc0c5b6aadd28683a8ead77fa01b3dc2b1b9
Aldar Dzhimbo <125662994+Dzhimbo@users.noreply.github.com>
2023-04-14 03:50:12 +0300

    Update and rename LABA 2.py to LABA 3.py

--- LABA 3.py
+++ LABA 3.py
@@ -28,6 +28,30 @@ def p2():
     else:
         print("Эх, это не очень редкое слово...")
 
+# Задание (3.4) 
+def p3()
+        import random
+        def generate_expression():
+            a = random.randint(1, 10)
+            b = random.randint(1, 10)
+            expression = f"{a} + {b} = "
+            answer = a + b
+            return expression, answer
 
+
+        correct_answers = 0
+        mistake_count = 0
+        while mistake_count < 3:
+            expression, answer = generate_expression()
+            user_answer = input(expression)
+            if int(user_answer) == answer:
+                print("Правильно!")
+                correct_answers += 1
+            else:
+                print("Ответ неверный")
+                mistake_count += 1
+      print(f"Игра окончена. Правильных ответов: {correct_answers}")
+    
 p1()
 p2()
+p3()
